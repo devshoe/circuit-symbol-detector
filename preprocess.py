8c1321ab8aad3355487966fba1a6712c059678ec
devshoe <devshoe@hotmail.com>
2021-05-24 21:49:32 +0530

    changes

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,7 @@ import sys
 import easyocr
 import keras
 import shutil
+# from prettyprint import pp
 
 #np.set_printoptions(threshold=sys.maxsize)
 CLASS_MAP = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabdefghnqrt'
@@ -25,7 +26,8 @@ def img_load(image_path):
       trans_mask = start_image[:,:,3] == 0 #mask wherever alpha is 0
       start_image[trans_mask] = [255, 255, 255, 255]
       start_image = cv2.cvtColor(start_image, cv2.COLOR_BGRA2BGR)
-    start_image = cv2.resize(start_image,(500,500))
+    w,h = start_image.shape[0],start_image.shape[1]
+    start_image = cv2.resize(start_image,(int(h/4),int(w/4)))
     img = start_image
     if len(img.shape)==3:
         grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -81,11 +83,7 @@ def get_contour_mask(img, reject_area_below=25):
 
     return component_cnt,text_cnts
 
-def mask():
-    comp,text = get_contour_mask(i[2])
-    img = np.zeros((500,500),"uint8")
-    cv2.drawContours(img, text, -1, 255,cv2.FILLED)
-    print(len(text))
+
     
 def extract_circuit(img):
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img,connectivity=4)
@@ -140,15 +138,6 @@ def predict_single_char_model(img):
     print(max(prediction))
     return CLASS_MAP[prediction.index(max(prediction))]
 
-def scale_contour(cnt, scale=1):
-    M = cv2.moments(cnt)
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
-
-    cnt_norm = cnt - [cx, cy]
-    cnt_scaled = cnt_norm * scale
-    cnt_scaled = cnt_scaled + [cx, cy]
-    return cnt_scaled.astype("uint8")
 
 def separated_chars_and_predict(img):
     contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)    
@@ -160,6 +149,156 @@ def separated_chars_and_predict(img):
         cv2.waitKey()
         print(predict_single_char_model(img))
 
+def length(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+def find_lines(img, vert_group_size=25, hor_group_size=25):
+    canny = cv2.Canny(img, 100,200)
+    height,width = img.shape[0], img.shape[1]
+    hline_thresh = 80
+    hline_min_line_length = min(height, width)*0.1
+    hline_max_line_gap = min(height, width)*0.1
+    all_lines = cv2.HoughLinesP(canny, 1, np.pi/180, 
+                           hline_thresh, None, 
+                           hline_min_line_length, hline_max_line_gap) #TODO
+    lines = []
+    for i,line in enumerate(all_lines):
+      for x1,y1,x2,y2 in line:
+        length= math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        slope = (y2-y1)/(x2-x1)
+        center = (int((x1+x2)/2),int((y1+y2)/2))
+        orientation = ""
+        grouping = 0
+        if abs(slope) != math.inf: 
+            slope = round(slope)
+            if slope ==0: 
+                orientation = "horizontal"
+                grouping = center[1] - (center[1]%hor_group_size)
+            else:
+                orientation = "vertical"
+                grouping = center[0] - (center[0]%vert_group_size)
+        else:
+            orientation = "vertical"
+            grouping = center[0] - (center[0]%vert_group_size)
+        
+        line_info = {
+            "label":f"B{i}", 
+            "coordinates":(x1,y1,x2,y2), 
+            "length":length, 
+            "center":center,
+            "slope": slope,
+            "orientation":orientation,
+            "grouping":grouping,
+            "components":[], #like ["4uH", "10R"]
+            "intersections": [], #like ["b1","b3"]
+            "merge": [],
+            }
+        lines.append(line_info)
+    return line_cleanup(lines)
+
+def draw_lines(img, lines, thickness=5):
+    for i, line in enumerate(lines):
+        x1,y1,x2,y2 = line["coordinates"]
+        cv2.line(img,(x1,y1),(x2,y2),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),thickness)
+        cv2.putText(img, f"B{i}", line["center"], cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),5)
+    return img
+'''
+okay to merge lines, they must have same orientation, ie angle < 10 degrees
+
+'''
+def line_cleanup(lines):
+    cleaned = []
+    grouped = {}
+    if len(lines)<2:
+        return lines
+    vertical_lines = [l for l in lines if l["orientation"] in ["angled","vertical"]]
+    vertical_groups = {}
+    horizontal_lines = [l for l in lines if l["orientation"]=="horizontal"]
+    horizontal_groups = {}
+    for k in vertical_lines:
+        if k["grouping"] not in vertical_groups.keys():
+            vertical_groups[k["grouping"]] = [k]
+        else:
+            vertical_groups[k["grouping"]].append(k)
+    for k in horizontal_lines:
+        if k["grouping"] not in horizontal_groups.keys():
+            horizontal_groups[k["grouping"]] = [k]
+        else:
+            horizontal_groups[k["grouping"]].append(k)
+
+    for group,lines in vertical_groups.items():
+        x,y = [],[]
+        for l in lines:
+            x.append(l["coordinates"][0])
+            x.append(l["coordinates"][2])
+            y.append(l["coordinates"][1])
+            y.append(l["coordinates"][3])
+        avg = int(sum(x)/len(x))
+        line_info = {
+            "label":f"B{len(cleaned)}", 
+            "coordinates":(avg,min(y),avg,max(y)), 
+            "center":(avg, int((min(y)+max(y))/2)),
+            "components":[], #like ["4uH", "10R"]
+            "intersections": [], #like ["b1","b3"]
+            "merge": [],
+            }
+        cleaned.append(line_info)
+    print(len(vertical_groups), vertical_groups.keys())
+    for group,lines in horizontal_groups.items():
+        x,y = [], []
+        for l in lines:
+            x.append(l["coordinates"][0])
+            x.append(l["coordinates"][2])
+            y.append(l["coordinates"][1])
+            y.append(l["coordinates"][3])
+        avg = int(sum(y)/len(y))
+        line_info = {
+            "label":f"B{len(cleaned)}", 
+            "coordinates":(min(x),avg,max(x),avg), 
+            "center":(int((min(x)+max(x))/2), avg),
+            "components":[], #like ["4uH", "10R"]
+            "intersections": [], #like ["b1","b3"]
+            "merge": [],
+            }
+        cleaned.append(line_info)
+    return cleaned
+
+def map_comps_to_branches(comps, branches):
+    for comp in comps:
+        closest,amt=0,1000000
+        for j,b in enumerate(branches):
+            l = length(comp["center"][0],comp["center"][1],b["center"][0], b["center"][1])
+            if l<amt:
+                amt = l
+                closest = j
+        branches[closest]["components"].append(comp)
+    return branches
+
+def intersections(branches):
+    for i,b1 in enumerate(branches):
+        x1,y1,x2,y2 = b1["coordinates"]
+        for j,b2 in enumerate(branches):
+            if j==i: continue
+            x3,y3,x4,y4 = b2["coordinates"]
+            l11 = point_is_on_line(x1,y1,x2,y2,x3,y3)
+            l12 = point_is_on_line(x1,y1,x2,y2, x4,y4)
+            if l11 or l12:
+                branches[i]["intersections"].append(b2["label"])
+    return branches
+
+def point_is_on_line(lx1,ly1,lx2,ly2,x,y,tolerance=30):
+  return (x<max(lx1,lx2)+tolerance and x>min(lx1,lx2)-tolerance) and (y<max(ly1,ly2)+tolerance and y>min(ly1,ly2)-tolerance)
+
+def connect_matrix(branches):
+    mat = {b["label"]:b for b in branches}
+    conn = {}
+    for k,v in mat.items():
+        for comp in v["components"]:
+            conn[comp["text"]] = []
+            for i in v["intersections"]:
+                val = [j["text"] for j in mat[i]["components"]]
+                conn[comp["text"]] += val
+    return conn
 def full(imgpath):
     try: 
         shutil.rmtree("results")
@@ -167,16 +306,26 @@ def full(imgpath):
         pass
     os.mkdir("results")
     i=img_load(imgpath)  
+    cpy = i[0].copy()
     ckt = extract_circuit(i[2])
-    pts, img = easyocr_get_centroids(i[0])
-    cv2.imwrite("results/0easyocr.jpg", img)
+
     cv2.imwrite("results/0original.jpg", i[0])
     cv2.imwrite("results/1grayscaled.jpg",i[1])
     cv2.imwrite("results/2thresholded.jpg",i[2])
     cv2.imwrite("results/3text.jpg", ckt[1])
     cv2.imwrite("results/3circuit.jpg", ckt[0])
-
-
+    lines = find_lines(i[0])
+    cv2.imwrite("results/4lines.jpg",draw_lines(i[0],lines))
+    pts, img = easyocr_get_centroids(cpy)
+    b = intersections(map_comps_to_branches(pts, lines))
+    print("branch connections:")
+    connections = {x["label"]:x["intersections"] for x in b}
+    print(connections)
+    print("components mapped to branches:")
+    comps = {x["label"]:[l["text"] for l in x["components"]] for x in b}
+    print(comps)
+    print(connect_matrix(b))
+    cv2.imwrite("results/0easyocr.jpg", img)
 
 if __name__ == "__main__":
-    full("data/hand2.jpeg")
+    full("data/2.jpeg")
